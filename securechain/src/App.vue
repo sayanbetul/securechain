76873c29bfff0301c649642b4a4ef3760f1a4ab6
sayanbetul <betulsayan0@gmail.com>
2024-09-20 16:28:59 +0800

    Fix line endings

---
+++ securechain/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <SecureChain />
+  </div>
+</template>
+
+<script>
+import SecureChain from './components/SecureChain.vue';
+
+export default {
+  name: 'App',
+  components: {
+    SecureChain,
+  },
+};
+</script>
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ securechain/src/components/SecureChain.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="secure-chain">
+    <h1>SecureChain</h1>
+    <div v-if="isAuthenticated">
+      <p>Welcome, {{ user.name }}!</p>
+      <p>Your security score: {{ securityScore }}</p>
+      <button @click="logout">Logout</button>
+    </div>
+    <div v-else>
+      <h2>Please log in</h2>
+      <form @submit.prevent="login">
+        <input type="text" v-model="username" placeholder="Username" required />
+        <input type="password" v-model="password" placeholder="Password" required />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isAuthenticated: false,
+      user: {
+        name: '',
+      },
+      securityScore: 0,
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    login() {
+      // Mock authentication logic
+      if (this.username === 'user' && this.password === 'password') {
+        this.isAuthenticated = true;
+        this.user.name = this.username;
+        this.securityScore = Math.floor(Math.random() * 100); // Simulated score
+      } else {
+        alert('Invalid credentials');
+      }
+    },
+    logout() {
+      this.isAuthenticated = false;
+      this.username = '';
+      this.password = '';
+      this.securityScore = 0;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.secure-chain {
+  text-align: center;
+}
+input {
+  margin: 5px;
+}
+button {
+  margin-top: 10px;
+}
+</style>
